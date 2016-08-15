2d1503ed90882eae9446ada2dcdc78a637e70c6d
j00302280 <j00101220@huawei.com>
2016-08-15 18:57:26 +0800

    upload the protocol stack seed code
    Change-Id: I86cf9ff7338e7860623417bf2faebcebd0dfaa1e
    Signed-off-by: j00302280 <j00101220@huawei.com>

---
+++ service/src/main/java/org/openo/gso/commsvc/connector/netconf/service/svc/business/INetconfMsgBusiness.java
@@ -0,0 +1,24 @@
+﻿package org.openo.gso.commsvc.connector.netconf.service.svc.business;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.openo.baseservice.remoteservice.exception.ServiceException;
+import org.openo.gso.commsvc.connector.netconf.service.svc.model.NetconfReqMessage;
+import org.openo.gso.commsvc.connector.netconf.service.svc.model.NetconfResponse;
+
+
+public interface INetconfMsgBusiness
+{
+    /**
+     * <br>
+     * @author
+     * @see 
+     * @since
+     * @param controllerId
+     * @param message 
+     * @param httpContext 
+     * @return 
+     * @throws ServiceException 
+     */
+    public NetconfResponse dispatchMessage(String controllerId, NetconfReqMessage message, HttpServletRequest httpContext) throws ServiceException;
+}
---
+++ service/src/main/java/org/openo/gso/commsvc/connector/netconf/service/svc/protocol/datadefination/IRpc.java
@@ -0,0 +1,60 @@
+﻿
+package org.openo.gso.commsvc.connector.netconf.service.svc.protocol.datadefination;
+
+import org.openo.gso.commsvc.connector.netconf.service.svc.protocol.error.NetconfException;
+
+/**
+ *  <br>
+ * @author
+ * @since  
+ */
+public interface IRpc
+{
+    /**
+     * <br>
+     * @author
+     * @since  
+     * @return
+     */
+    String getContent();
+
+    /**
+     * <br>
+     * @author
+     * @since  
+     * @return lrID
+     */
+    Integer getLrID();
+
+    /**
+     *  <br>
+     * @author
+     * @since  
+     * @return
+     */
+    long getTimeout();
+
+    /**
+     *  <br>
+     * @author
+     * @since 
+     * @return
+     */
+    String getCharset();
+
+    /**
+     *  <br>
+     * @author
+     * @since  
+     * @param charset
+     */
+    void setCharset(String charset);
+
+    /**
+     * <br>
+     * @author
+     * @since  
+     * @return vrID
+     */
+    Integer getVrID();   
+}
